9f5ec17de492133b581d64dfde30bfb787306083
Eliana Barrios <elianabarrios9827@gmail.com>
2022-10-20 23:47:30 -0300

    level-options

---
+++ scenes/level-party/LevelParty.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+var res = 0
+
+
+func _ready():
+	pass 
+
+
+func _on_RespButton_pressed():
+	pass
+
+
+func _on_RespButton2_pressed():
+	pass
+
+
+func _on_RespButton3_pressed():
+	pass
+
+
+
+func _on_RespButton4_pressed():
+	pass
+
+
+func _on_RespButton5_pressed():
+	pass
+
+func _on_RespButton6_pressed():
+	pass
