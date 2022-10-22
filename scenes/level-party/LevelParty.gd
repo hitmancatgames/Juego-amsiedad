19bf71d626193ca78359fe202069e351d61c34f6
Eliana Barrios <elianabarrios9827@gmail.com>
2022-10-21 23:02:06 -0300

    buttons

--- scenes/level-party/LevelParty.gd
+++ scenes/level-party/LevelParty.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var res = 0
+var correct = false
 
 
 func _ready():
@@ -9,19 +9,44 @@ func _ready():
 
 
 func _on_RespButton_pressed():
+	correct == true
+	change_dialog()
+	$HBoxContainer/RespButton.hide
 	pass
 
 func _on_RespButton2_pressed():
+	correct == false
+	change_dialog()
+	$HBoxContainer/RespButton2.hide
 	pass
 
 func _on_RespButton3_pressed():
+	correct == false
+	change_dialog()
+	$HBoxContainer/RespButton3.hide
 	pass
 
 func _on_RespButton4_pressed():
+	correct == false
+	change_dialog()
+	$HBoxContainer/RespButton4.hide
 	pass
 
 func _on_RespButton5_pressed():
+	correct == false
+	change_dialog()
+	$HBoxContainer/RespButton5.hide
 	pass
 
 func _on_RespButton6_pressed():
+	correct == false
+	change_dialog()
+	$HBoxContainer/RespButton6.hide
+	pass
+
+func change_dialog():
+	if correct == true:
+		$FloppaDialog.text = "Feedback correcto"
+	else: 
+		$FloppaDialog.text = "Feedback incorrecto"
 	pass
