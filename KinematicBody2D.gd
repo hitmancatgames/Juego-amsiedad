f2e3915543af72bd29d614d40e92de5dbd904198
Milo Vargas <80860312+MiloVarok@users.noreply.github.com>
2022-11-13 16:26:32 -0300

    street-level-basic-movement
    Se hizo el movimiento del personaje con sus colisiones, los limites del mapa, texto de introducción, edición de la barra de amsiedad

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,20 +1,27 @@
 extends KinematicBody2D
 
 
-#var movement = Vector2(0,-200)
-#
-#
-#const VECTOR_NORMAL = Vector2(-1, 0)
-#
-#func _ready():
-#	pass 
-#func _physics_process(delta):
-#
-#	move_and_slide(movement, VECTOR_NORMAL)
-#	pass
+var vel_run = 400
+var velocity = Vector2()
+
+func _get_input():
+	velocity.x = 0
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	
+	if right:
+		velocity.x += vel_run
+		$Cheems.flip_h = velocity.x < 0
+	elif left:
+		velocity.x -= vel_run
+		$Cheems.flip_h = velocity.x < 0
+		
+
+func _physics_process(delta):
+	_get_input()
+	velocity= move_and_slide(velocity, Vector2(0,0))
 
 
-func _on_Area2D_body_exited(body):
-	
-	
-	pass
+func _on_Area2D_area_entered(area):
+	get_tree().call_group("Street","Anxiety")
+	pass 
--- scenes/street/Street.gd
+++ scenes/street/Street.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
-func modify():
-	get_tree().call_group("enemy" , "")
+var AnxietyLevel = 0
+var AnxietyMax = 50
+
+
+func _ready():
+	
+	pass
+
+
+func _anxiety():
+	if AnxietyLevel < AnxietyMax:
+		AnxietyLevel = AnxietyLevel + 10
+		$AnxietyBar.value = AnxietyLevel
+	elif AnxietyLevel >= AnxietyMax:
+		print("game over")
+	pass
+
+
+func _on_Timer_timeout():
+	$AnxietyDialog.hide()
 	pass
